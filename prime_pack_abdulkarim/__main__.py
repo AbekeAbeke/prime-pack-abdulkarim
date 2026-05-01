4bbe6e5648ebb5f68d16066017f83e683cc624d2
Abeke <Abeke@MacBook-Air-2.local>
2026-05-02 01:27:17 +0500

    save current working version

--- prime_pack_abdulkarim/__main__.py
+++ prime_pack_abdulkarim/__main__.py
@@ -3,9 +3,9 @@ import random
 from . import primes, checksum
 
 @click.command()
-@click.option('--count', default=1000, help='Number of primes')
-@click.option('--seed', default=100, help='Random seed')
-def main(count: int, seed: int):
+@click.option('--count', default=1000)
+@click.option('--seed', default=100)
+def main(count, seed):
     nums = primes(count)
     random.seed(seed)
     random.shuffle(nums)
